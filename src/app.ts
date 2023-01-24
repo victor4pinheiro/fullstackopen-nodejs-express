e254a9535565682e18de543eb7abb9998e94c3d4
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-01-24 12:05:47 -0300

    chore: add app.use to use json and router

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
-const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.json());
+app.use(router);
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+export { app };
