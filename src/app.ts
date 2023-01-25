ff8e88d22dc6c7bd42f13f1d679bd50bc2c36250
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-01-24 23:16:53 -0300

    chore: add cors to enable CORS and morgan to log to stdout

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { router } from "./routes";
-
+import cors from "cors";
+import morgan from "morgan";
 const app = express();
 
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 app.use(router);
+app.use(cors);
+
+morgan.token("body", function (request) {
+  return JSON.stringify(request["body"]);
+});
 
 export { app };
