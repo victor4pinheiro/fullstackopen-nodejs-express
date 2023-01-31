00832b8f7cb9c39b3696c3e63267791a6ca10131
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-01-31 00:13:58 -0300

    refactor: using promises and handle errors Originally, I was using try/catch, but this make the code more larger and polluted. To reduce it, I refactored to use promises. But it's not enought, because handling error always make the code larger, so I created (requirements of project) a middleware named handleError to handle this errors and make the code less polluted.

--- src/controllers/PersonController.ts
+++ src/controllers/PersonController.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import Database from "../database/database";
+import CustomError from "../model/errors";
 import Person from "../model/person";
 
 interface PersonInterface {
@@ -17,88 +18,108 @@ class PersonController {
     return await Person.find().countDocuments();
   }
 
-  create(request: Request, response: Response) {
+  create(request: Request, response: Response, next: NextFunction) {
     const body = request.body;
 
     if (!PersonController.isPerson(body))
-      return response.status(404).json({ error: "Missing name or number" });
-
-    /* if (PersonController.newPersons.find((person) => person.name === body.name))
-      return response.status(409).json({ error: "Person already exists" }); */
+      next(new CustomError("missing name or number", 404));
 
     const person = new Person({
       ...body,
     });
 
-    try {
-      Database.connect().then(() => {
-        person.save().then((savedPerson) => {
-          response.status(200).json(savedPerson);
-          Database.disconnect();
-        });
-      });
-    } catch (error) {
-      response.status(400).json({ error: `Failed operation: ${error}` });
-    }
+    Database.connect()
+      .then(() => {
+        person
+          .save()
+          .then((person) => response.status(201).json(person))
+          .catch((error) => next(error))
+          .finally(() => mongoose.disconnect());
+      })
+      .catch((error) => next(error));
   }
 
-  getAll(request: Request, response: Response) {
-    try {
-      Database.connect().then(() => {
-        Person.find({}).then((notes) => {
-          response.status(200).json(notes);
-          Database.disconnect();
-        });
-      });
-    } catch (error) {
-      response.status(400).json({ error: `Failed operation: ${error}` });
-    }
+  getAll(request: Request, response: Response, next: NextFunction) {
+    Database.connect()
+      .then(() => {
+        Person.find({})
+          .then((persons) => response.status(200).json(persons))
+          .catch((error) => next(error))
+          .finally(() => mongoose.disconnect());
+      })
+      .catch((error) => next(error));
   }
 
-  getById(request: Request, response: Response) {
+  getById(request: Request, response: Response, next: NextFunction) {
     const id = request.params.id;
 
-    try {
-      Database.connect().then(() => {
+    Database.connect()
+      .then(() => {
         Person.findById(id)
-          .then((person) => {
-            return response.json(person);
-          })
-          .catch((error) => {
-            return response.status(404).json({ error: "No person found" });
-          });
-        Database.disconnect();
-      });
-    } catch (error) {
-      response.status(400).json({ error: `Failed operation: ${error}` });
-    }
+          .then((person) =>
+            person
+              ? response.status(200).json(person)
+              : next(new CustomError("person not found", 404))
+          )
+          .catch((error) => next(error))
+          .finally(() => mongoose.disconnect());
+      })
+      .catch((error) => next(error));
   }
 
-  getInfo(request: Request, response: Response) {
-    Database.connect().then(async () => {
-      const count = await Person.countDocuments();
-      return response.send(
-        `Phonebook has info for ${count} persons. <br/>${new Date()}`
-      );
-    });
+  getInfo(request: Request, response: Response, next: NextFunction) {
+    Database.connect()
+      .then(async () => {
+        const count = await Person.countDocuments();
+        return response
+          .status(200)
+          .send(`Phonebook has info for ${count} persons. <br/>${new Date()}`);
+      })
+      .catch((error) => next(error));
   }
 
-  delete(request: Request, response: Response) {
+  update(request: Request, response: Response, next: NextFunction) {
+    const body = request.body;
+
+    if (!PersonController.isPerson(body))
+      next(new CustomError("missing name or number", 404));
+
     const id = request.params.id;
 
-    console.log(typeof id);
-    try {
-      Database.connect().then(() => {
-        Person.findByIdAndDelete(id, (error, person) => {
-          Database.disconnect();
-          if (error)
-            return response.status(404).json({ error: `Person not found` });
-          return response.status(200).json(person);
-        });
-      });
-    } catch (error) {
-      response.status(400).json({ error: `Failed operation: ${error}` });
-    }
+    const person = new Person({
+      _id: id,
+      ...body,
+    });
+
+    Database.connect()
+      .then(() => {
+        Person.findByIdAndUpdate(id, person)
+          .then((person) =>
+            person
+              ? response.status(200).json(person)
+              : next(new CustomError("person not found", 404))
+          )
+          .catch((error) => next(error))
+          .finally(() => mongoose.disconnect());
+      })
+      .catch((error) => next(error));
+  }
+
+  delete(request: Request, response: Response, next: NextFunction) {
+    const id = request.params.id;
+
+    Database.connect()
+      .then(() => {
+        Person.findByIdAndDelete(id)
+          .then((person) =>
+            person
+              ? response.status(200).json(person)
+              : next(new CustomError("person not found", 404))
+          )
+          .catch((error) => next(error))
+          .finally(() => mongoose.disconnect());
+      })
+      .catch((error) => next(error));
   }
 }
 
