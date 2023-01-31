20338c103ee4e95f43ac4956bda36c786425cb40
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-01-31 00:19:31 -0300

    refactor: return the connection of mongoose instead of a promise

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,11 +4,8 @@ const url = process.env.MONGODB_URI;
 
 class Database {
   static async connect() {
-    return await mongoose.connect(url);
-  }
-
-  static async disconnect() {
-    return await mongoose.disconnect();
+    const client = await mongoose.connect(url);
+    return client.connection;
   }
 }
 
