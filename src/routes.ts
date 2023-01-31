8b84be411c45462fb2d4b87cfae7f5a548fad9d6
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-01-31 00:21:26 -0300

    feat: add update route

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ router.post("/api/persons", personController.create);
 router.get("/api/persons", personController.getAll);
 router.get("/api/persons/:id", personController.getById);
 router.get("/api/info", personController.getInfo);
+router.put("/api/persons/:id", personController.update);
 router.delete("/api/persons/:id", personController.delete);
 
 export { router };
